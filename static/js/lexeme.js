56b94ef96cb5f3ae3bbdf0c446f20f806be87eca
Jirka Balhar <balhar.j@gmail.com>
2016-02-08 21:11:44 +0100

    Odstranění scroll animace dělala samé potíže

--- static/js/lexeme.js
+++ static/js/lexeme.js
@@ -262,10 +262,7 @@ var LexemesView = Backbone.View.extend({
 		});
 
 		if(first !== undefined){
-			this.$el.find(".result").mCustomScrollbar("scrollTo", "."+first[1].parent.id, {
-				scrollInertia: 1000,
-				callbacks: true
-			});
+			this.$el.find(".result").mCustomScrollbar("scrollTo", "."+first[1].parent.id);
 		}
 	},
 
