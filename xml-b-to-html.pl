23a5601795b6a0cc51a25049d3c5bae8501c8169
Jirka Balhar <balhar.j@gmail.com>
2016-06-25 16:47:22 +0200

    Oprava zalamování řádků u more/less

--- xml-b-to-html.pl
+++ xml-b-to-html.pl
@@ -407,7 +407,7 @@ sub pdtvallex_word_links {
     $url_lemma =~ s/\ /+/x;
     $wlink_string .=
       "<li><a href='$PDTVALLEX_URL$url_lemma' target='_blank' class='external'>"
-      . "<span>$lemma</span>"
+      . "<span>$lemma</span>&nbsp;"
       . "<div class='arrow'>&gt;</div></a></li>";
   }
   if($wlink_string){
